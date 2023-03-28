6e3a4f770464ed3a62b4a9ce4cb1fc2ca340520e
soultan.ali.h <soultan.ali.h@gmail.com>
2023-03-28 21:38:29 +0700

    set github pages

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="app">
       <Router>
         <Switch>
-          <Route exact path="https://SoultanAliHadji.github.io/web_certification" component={Navbar} />
+          <Route exact path="https://soultanalihadji.github.io/web_certification/" component={Navbar} />
         </Switch>
       </Router>
     </div>
